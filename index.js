ef46178d7edd7d37398709ef14d5fac4daf5a6b7
erikchiodo <erikchiodo@gmail.com>
2023-04-04 21:23:03 -0400

    Finished prompts, wrote function for writing file and initializng

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const userPrompts = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter title of your project: ",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter description: ",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instruction: ",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information: ",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contribution guidelines: ",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter test instructions: ",
+  },
+  {
+    type: "input",
+    name: "reponame",
+    message: "Enter repo name: ",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Enter Github username: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address: ",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license",
+    choices: [
+      { name: "No License", value: "" },
+      "Academic Free License v3.0",
+      "Apache license 2.0",
+      "BSD 3-clause Clear license",
+      "Creative Commons license family",
+      "GN General Public License v3.0",
+      "ISC",
+      "MIT",
+      "Open Software License 3.0",
+    ],
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file
 
-// TODO: Create a function to initialize app
-function init() {}
+async function writeFile(fileName, data) {
+  try {
+    let filePath = path.join(__dirname, "/ReadMe/", fileName);
+    await fs.writeFile(filePath, data);
+    console.log("Readme generated successfully");
+  } catch (error) {
+    console.log("Error generating File");
+    console.error(error);
+  }
+}
 
-// Function call to initialize app
+function init() {
+  inquirer
+    .prompt(userPrompts)
+    .then((userResponses) => {
+      const readMeData = generateMarkdown(userResponses);
+      writeFile("README.md", readMeData);
+    })
+    .catch((err) => console.error(err));
+}
+
+// function call to initialize app
 init();
